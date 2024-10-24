be10a74ac1084f9eb49ae17c1fa025bd64088d47
Sania Singla <singlasania7717@gmail.com>
2024-10-24 21:23:56 +0530

    new web project completed

--- WebProject/script.js
+++ WebProject/script.js
@@ -1,17 +1,32 @@
+import { icons } from "./icons.js";
+
 let fullname = document.getElementById("fullname");
 let username = document.getElementById("username");
 let password = document.getElementById("password");
 let email = document.getElementById("email");
+let remarks = document.getElementById("remarks");
+
+let fullnameError = document.getElementById("fullname-error");
+let usernameError = document.getElementById("username-error");
+let passwordError = document.getElementById("password-error");
+let emailError = document.getElementById("email-error");
+let remarksError = document.getElementById("remarks-error");
+
+let genderRadios = document.getElementsByName("gender");
 let clearAllBtn = document.getElementById("clear-all-btn");
+
 let countryList = [];
 let countrySelect = document.getElementById("country");
 let flagImage = document.getElementById("flag-image");
 let resetBtn = document.getElementById("reset-btn");
+let submitBtn = document.getElementById("submit-btn");
 let form = document.getElementById("form");
 let leadsDiv = document.getElementById("leads");
 let totalLeads = [];
 
-window.addEventListener("load", async (e) => {
+// HELPER FUNCTIONS (UTILS)
+
+async function fetchCountryList() {
     try {
         const res = await fetch("https://countriesnow.space/api/v0.1/countries/flag/images", {
             method: "GET",
@@ -20,36 +35,48 @@ window.addEventListener("load", async (e) => {
             throw new Error("Network response was not ok");
         }
         const result = await res.json();
-        countryList = result.data;
-        addCountryList();
-
-        const savedLeads = localStorage.getItem("leads");
-        totalLeads = savedLeads ? JSON.parse(savedLeads) : [];
-        if (totalLeads.length) {
-            totalLeads.forEach((lead) => {
-                const selectedCountry = countryList.find(
-                    (countryObject) => countryObject.name === lead?.country
-                );
-                console.log(selectedCountry);
-                const eachLead = document.createElement("div");
-                eachLead.innerHTML = `
-                                    <img src="${selectedCountry?.flag}" alt="${lead?.country} flag"/>
-                                    <p class="fullname">${lead?.fullname}</p>
-                                    <p class="username">&bull; @${lead?.username}</p>
-                                    <p class="email">${lead?.email}</p>
-                                `;
-                eachLead.setAttribute("class", "lead");
-                leadsDiv.append(eachLead);
-            });
-        }
+        return result.data;
     } catch (error) {
         console.error("Error fetching country data:", error);
     }
-});
+}
+
+function updateFlag() {
+    const selectedCountry = countryList.find(
+        (countryObject) => countryObject.name === countrySelect.value
+    );
+    flagImage.setAttribute("src", selectedCountry.flag);
+    flagImage.setAttribute("alt", `${selectedCountry.name} flag`);
+}
+
+function resetValuesAndFlag() {
+    fullname.value = "";
+    username.value = "";
+    email.value = "";
+    password.value = "";
+    remarks.value = "";
+    countrySelect.value = "India";
+    genderRadios.forEach((radio) => (radio.checked = false));
+    updateFlag();
+}
+
+function hasErrors() {
+    if (
+        fullnameError.innerText ||
+        usernameError.innerText ||
+        passwordError.innerText ||
+        emailError.innerText ||
+        remarksError.innerText
+    ) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 function addCountryList() {
     countryList.forEach((countryObject) => {
-        const { name, flag } = countryObject;
+        const { name } = countryObject;
         const option = document.createElement("option");
         option.innerText = name;
         option.setAttribute("value", name);
@@ -61,67 +88,174 @@ function addCountryList() {
     updateFlag();
 }
 
-countrySelect.addEventListener("change", updateFlag);
-
-function updateFlag() {
-    const selectedCountry = countryList.find(
-        (countryObject) => countryObject.name === countrySelect.value
-    );
-
-    flagImage.setAttribute("src", selectedCountry.flag);
-    flagImage.setAttribute("alt", `${selectedCountry.name} flag`);
+function setLeadHTMLString(flag, country, email, username, fullname, gender) {
+    return `
+            <img src="${flag}" alt="${country} flag"/>
+            <p class="fullname">${fullname}</p>
+            <p class="username">&bull; @${username}</p>
+            <p class="email">${email}</p>
+            <div class="gender-icon-div">${
+                gender === "female" ? icons.female : gender === "male" ? icons.male : icons.other
+            }</div>
+        `;
 }
+
+// EVENT LISTENERS
+
+window.addEventListener("load", async (e) => {
+    countryList = await fetchCountryList();
+    addCountryList();
+
+    const savedLeads = localStorage.getItem("leads");
+    totalLeads = savedLeads ? JSON.parse(savedLeads) : [];
+    if (totalLeads.length) {
+        totalLeads.forEach((lead) => {
+            const { fullname, username, email, gender, country } = lead;
+            const selectedCountry = countryList.find(
+                (countryObject) => countryObject.name === country
+            );
+            const eachLead = document.createElement("div");
+            eachLead.innerHTML = setLeadHTMLString(
+                selectedCountry?.flag,
+                country,
+                email,
+                username,
+                fullname,
+                gender
+            );
+            eachLead.setAttribute("class", "lead");
+            leadsDiv.append(eachLead);
+        });
+    }
+});
+
+countrySelect.addEventListener("change", updateFlag);
 
 resetBtn.addEventListener("click", (e) => {
     e.preventDefault();
-    fullname.value = "";
-    username.value = "";
-    email.value = "";
-    password.value = "";
-    countrySelect.value = "India";
-    updateFlag();
+    resetValuesAndFlag();
 });
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    totalLeads = [
-        ...totalLeads,
-        {
-            fullname: fullname.value,
-            username: username.value,
-            email: email.value,
-            password: password.value,
-            country: countrySelect.value,
-        },
-    ];
-
-    localStorage.setItem("leads", JSON.stringify(totalLeads));
-
-    const selectedCountry = countryList.find(
-        (countryObject) => countryObject.name === countrySelect.value
-    );
-    const newLead = document.createElement("div");
-    newLead.innerHTML = `
-                        <img src="${selectedCountry?.flag}" alt="${countrySelect.value} flag"/>
-                        <p class="fullname">${fullname.value}</p>
-                        <p class="username">&bull; @${username.value}</p>
-                        <p class="email">${email.value}</p>
-                    `;
-
-    newLead.setAttribute("class", "lead");
-    leadsDiv.append(newLead);
-    fullname.value = "";
-    username.value = "";
-    email.value = "";
-    password.value = "";
-    countrySelect.value = "India";
-    updateFlag();
+submitBtn.addEventListener("mouseover", (e) => {
+    submitBtn.disabled = hasErrors();
 });
 
 clearAllBtn.addEventListener("click", (e) => {
     localStorage.removeItem("leads");
     while (leadsDiv.firstChild) {
-        // will run until we have a child (div.firstChild != null)
         leadsDiv.removeChild(leadsDiv.firstChild);
+    }
+});
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (hasErrors()) {
+        return;
+    } else {
+        let gender;
+        genderRadios.forEach((radio) => {
+            if (radio.checked) {
+                gender = radio.value;
+            }
+        });
+
+        totalLeads = [
+            ...totalLeads,
+            {
+                fullname: fullname.value,
+                username: username.value,
+                email: email.value,
+                password: password.value,
+                country: countrySelect.value,
+                remarks: remarks.value,
+                gender,
+            },
+        ];
+
+        localStorage.setItem("leads", JSON.stringify(totalLeads));
+        const selectedCountry = countryList.find(
+            (countryObject) => countryObject.name === countrySelect.value
+        );
+        const newLead = document.createElement("div");
+        newLead.innerHTML = setLeadHTMLString(
+            selectedCountry?.flag,
+            countrySelect.value,
+            email.value,
+            username.value,
+            fullname.value,
+            gender
+        );
+        newLead.setAttribute("class", "lead");
+        leadsDiv.append(newLead);
+        resetValuesAndFlag();
+    }
+});
+
+// DATA VALIDATION HANDLERS
+
+function validate(type, value) {
+    if (value && type) {
+        switch (type) {
+            case "fullname": {
+                return /^[a-zA-Z]{1,15}$/.test(value) ? true : false;
+            }
+            case "username": {
+                return value.length <= 12 ? true : false;
+            }
+            case "email": {
+                return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,100}$/.test(value)
+                    ? true
+                    : false;
+            }
+            case "password": {
+                return value.length > 8 || value.length < 12 ? true : false;
+            }
+            case "remarks": {
+                return value.length <= 256 ? true : false;
+            }
+            default: {
+                console.log("please provide a defined type to validate.");
+            }
+        }
+    }
+}
+
+fullname.addEventListener("blur", (e) => {
+    if (fullname.value && !validate("fullname", fullname.value)) {
+        fullnameError.innerText = `only letters are allowed.`;
+    } else {
+        fullnameError.innerText = "";
+    }
+});
+
+username.addEventListener("blur", (e) => {
+    if (username.value && !validate("username", username.value)) {
+        usernameError.innerText = `max length allowed is 12 char.`;
+    } else {
+        usernameError.innerText = "";
+    }
+});
+
+email.addEventListener("blur", (e) => {
+    if (email.value && !validate("email", email.value)) {
+        emailError.innerText = `please enter a valid email.`;
+    } else {
+        emailError.innerText = "";
+    }
+});
+
+password.addEventListener("blur", (e) => {
+    if (password.value && !validate("password", password.value)) {
+        passwordError.innerText = `password should be 8-12 char.`;
+    } else {
+        passwordError.innerText = "";
+    }
+});
+
+remarks.addEventListener("blur", (e) => {
+    if (remarks.value && !validate("remarks", remarks.value)) {
+        remarksError.innerText = `exceeded 256 char length is allowed`;
+    } else {
+        remarksError.innerText = "";
     }
 });
